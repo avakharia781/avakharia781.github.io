be842f571f1841e617dc55cdde130e93e4e7fe73
avakharia781 <58555625+avakharia781@users.noreply.github.com>
2020-05-10 16:46:55 +0530

    First WebSite Upload

---
+++ src/js/home.js
@@ -0,0 +1,19 @@
+window.onload = function() {
+    var typed = new Typed('#quotes', {
+        strings: [
+            "Debugging” is like being the detective in a crime drama where you are also the murderer.",
+            "The best thing about a Boolean is that even if you are wrong, you are only off by a bit.",
+            "An SQL query goes into a bar, walks up to two tables and asks: “Can I join you?”",
+            "Why do Java programmers have to wear glasses? Because they don’t C#.",
+            "I went to a street where the houses were numbered 8k, 16k, 32k, 64k, 128k, 256k and 512k. It was a trip down Memory Lane.",
+            "!false (It’s funny because it’s true.)"
+        ],
+        typeSpeed: 50,
+        backSpeed: 20,
+        backDelay: 2500,
+        smartBackspace: true,
+        cursorChar:"",
+        loop: true,
+        shuffle: true
+    });
+};
