e08b248b840dae5dd89adfc83341a7ca14e765d6
avakharia781 <58555625+avakharia781@users.noreply.github.com>
2020-06-13 20:12:01 +0530

    namechange

---
+++ myPage/src/js/myWork.js
@@ -0,0 +1,11 @@
+function getDeprApp(){
+    window.open("/myPage/projects/DepreciationCalc/index.html");
+}
+
+function getTodoApp(){
+    window.open("/myPage/projects/To-Do-List-master/index.html");
+}
+
+function getQuizApp(){
+    window.open("/myPage/projects/Quiz/index.html");
+}
